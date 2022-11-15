c7d89167cebdcd3f48f315ac48a10a07285fba41
Diego Sarasa <diegosarasa96@gmail.com>
2022-11-15 17:18:50 +0100

    Create Pruebas.py

---
+++ Pruebas.py
@@ -0,0 +1,31 @@
+saludo = 'Hola'
+print(saludo)
+
+x,y = int(5),3
+
+x = x + 1
+
+if x > y:
+    print('good')
+    print('\nok')
+
+#Oh my fucking God
+
+'''
+lalalalala
+'''
+
+def funcion():
+    x = 7
+    print(x,x,x,x)
+
+letras = ['a','b','c']
+
+print(letras[1])
+
+print('prueba',y+x)
+
+funcion()
+
+print(type(saludo))
+
